cfb5b06e58e5e1a0b3303192f50196286a991abc
unknown <binyaalex@gmail.com>
2021-10-10 14:55:35 +0300

    adding daily challenge

---
+++ week 2/day 1/daily challenge/script.js
@@ -0,0 +1,15 @@
+// exercise 1
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0, 1);
+fruits.sort();
+fruits.push(`kiwi`);
+fruits.shift();
+fruits.reverse();
+console.log(fruits)
+
+// exercise 2
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+Oranges = moreFruits[1][1][0];
+console.log(Oranges)
