79ff65432aa0d58d3cfeaf1e4a0ec2253dc35285
unknown <binyaalex@gmail.com>
2021-10-20 14:43:33 +0300

    adding mini-project

---
+++ week3/day4/exercise/script.js
@@ -0,0 +1,54 @@
+let sidebar = document.getElementById('sidebar');
+let main = document.getElementById('main');
+let colorArr = [`red`, `orangered`, `orange`, `yellow`, `yellowgreen`, `lightgreen`, `green`, `turquoise`, `cyan`, `lightskyblue`, `dodgerblue`, `blue`, `darkblue`, `indigo`, `darkmagenta`, `violet`, `lightpink`, `lightgray`, `gray`, `black`, `white`]
+let myColor
+let isMouseDown
+
+function pallet () {
+	for (let i = 0; i < colorArr.length; i++) {
+		let newD = document.createElement(`div`);
+		newD.style.backgroundColor = `${colorArr[i]}`;
+		newD.classList.add(`colorSqaure`)
+		newD.addEventListener(`click`, getColor)
+		sidebar.appendChild(newD)
+	}
+}
+
+pallet();
+
+function paintArea () {
+	for (let i = 0; i < 1440; i++) {
+		let newD = document.createElement(`div`);
+		newD.style.backgroundColor = `white`;
+		newD.classList.add(`whiteSqaure`);
+		newD.addEventListener(`click`, setColor);
+		newD.addEventListener(`mousedown`, MouseDown);
+		newD.addEventListener(`mouseover`, mouseCheck);
+		newD.addEventListener(`mouseup`, mouseUp);
+		main.appendChild(newD);
+	}
+}
+
+paintArea ();
+
+function getColor (e) {
+	myColor = e.target.style.backgroundColor
+}
+
+function setColor (e) {
+	e.target.style.backgroundColor = myColor
+}
+
+function MouseDown() {
+	isMouseDown = true;
+}
+
+function mouseCheck(e) {
+	if (isMouseDown) {
+		setColor(e)
+	}
+}
+
+function mouseUp () {
+	isMouseDown = false;
+}
