761a69d3f537a9523b57acf59e11af18c887ebaf
unknown <binyaalex@gmail.com>
2021-10-29 14:02:08 +0300

    fix

--- week4/day5/dc/script.js
+++ week4/day5/dc/script.js
@@ -1,13 +1,8 @@
 let d = `Here come dots`
 let b = `The Morse Code`
 const anagram = (string1, string2) => {
-  let string1Arr = string1.toLowerCase().split(``).sort()
-  let string2Arr = string2.toLowerCase().split(``).sort()
-  let i1 = string1Arr.lastIndexOf(` `)
-  string1Arr.splice(0, i1+1)
-  let i2 = string2Arr.lastIndexOf(` `)
-  string2Arr.splice(0, i2+1)
-  console.log(string2Arr.join() === string1Arr.join())
+  let string1Arr = string1.toLowerCase().split(``).sort().join(``).replace(/ /g, ``)
+  let string2Arr = string2.toLowerCase().split(``).sort().join(``).replace(/ /g, ``)
+  console.log(string2Arr == string1Arr)
 }
-
 anagram(d, b)
