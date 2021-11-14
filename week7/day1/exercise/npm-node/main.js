a7d6007c9b2c7ce571a15a5305d090257fdd3158
unknown <binyaalex@gmail.com>
2021-11-14 15:37:00 +0200

    add exercise

---
+++ week7/day1/exercise/npm-node/main.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log(`Hello you are ${a+b}`)
+console.log(`Hi, thanks for running me with npm start!`)
+console.log(`first run: ${a+b}`)
+console.log(`second run: ${x+c}`)
