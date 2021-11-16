33326a501349b9e308727c48e374d0b6885a0795
unknown <binyaalex@gmail.com>
2021-11-16 15:22:00 +0200

    add exercise

---
+++ week7/day3/exercise/fs.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+// fs.readFile('text', (err, data)=> {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log(data.toString())
+// 	}
+// })
+
+// fs.writeFile('data', 'bla bla', err=> {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log('finito')
+// 	}
+// })
+
+// fs.appendFile('data', '\n more bla bla', err=> {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log('finito')
+// 	}
+// })
+
+// fs.unlink('data', err=> {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log('deleto')
+// 	}
+// })
