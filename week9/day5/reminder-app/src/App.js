d6b9208d91bf877dbb7e12bb62755f8d21d9581b
unknown <binyaalex@gmail.com>
2022-01-06 15:17:12 +0200

    add h1

--- week9/day5/reminder-app/src/App.js
+++ week9/day5/reminder-app/src/App.js
@@ -5,8 +5,11 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <input type='text' placeholder='I have to...' />
-        <button>Add Reminder</button>
+        <h1 className='title'>Reminder App</h1>
+        <div className='inputs'>
+          <input type='text' placeholder='I have to...' />
+          <button>Add Reminder</button>
+        </div>
       </div>
     );  
   }
