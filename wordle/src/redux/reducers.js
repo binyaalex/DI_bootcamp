f77b859191acaf8793a2a5ae1145e62a21f8e99c
unknown <binyaalex@gmail.com>
2022-01-10 14:44:09 +0200

    enter button

--- wordle/src/redux/reducers.js
+++ wordle/src/redux/reducers.js
@@ -1,6 +1,7 @@
 const initState = {
 		dailyWord: 'raise',
-		userWord: ''
+		userWord: '',
+		result: ['gray','gray','gray','gray','gray']
 	}
 
 
@@ -11,16 +12,19 @@ export const reducer = (state=initState, action={}) => {
 		  return {...state, userWord: action.payload}
 		case 'ENTER':
 		console.log(state)
+		  let arr = []
 		  for (let i = 0; i < state.userWord.length; i++) {
 		  	for (let d = 0; d < state.dailyWord.length; d++) {
 		  		if (state.userWord[i] === state.dailyWord[d] && d === i) {
 		  		    console.log(state.userWord[i],': green')
-		  	    } else (state.userWord[i] === state.dailyWord[d]) {
+		  		    arr[i] = 'green'
+		  	    } else if (state.userWord[i] === state.dailyWord[d]) {
 		  	    	console.log(state.userWord[i],': yellow')
+		  		    arr[i] = 'yellow'
 		  	    }
 		  	}
 		  }
-		  return {...state}
+		  return {...state, result: arr}
 	  	default:
 	  	  return {...state}
 	}
