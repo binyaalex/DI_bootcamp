a2c801b4e85cdfb3c10bf7c6951e31b2a974a221
unknown <binyaalex@gmail.com>
2022-02-02 13:28:18 +0200

    design results squers

--- wordle/src/components/Result.js
+++ wordle/src/components/Result.js
@@ -1,8 +1,52 @@
 const Result = (props) => {
 	return (
-		<>
-			s
-		</>
+		<div className='result'>
+			<h1>Result</h1>
+			<div className='resultSquers'>
+				<div className='resultLine'>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+				</div>
+				<div className='resultLine'>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+				</div>
+				<div className='resultLine'>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+				</div>
+				<div className='resultLine'>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+				</div>
+				<div className='resultLine'>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+				</div>
+				<div className='resultLine'>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+					<div className='resultSquer'></div>
+				</div>
+			</div>
+		</div>
 	)
 }
 export default Result
